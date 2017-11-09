aad7f4670e72653f9d66221248daedc48a1521d4
Dave Gaeddert <dave.gaeddert@gmail.com>
2017-11-09 15:16:23 -0600

    add github.com netrc config

--- entrypoint.php
+++ entrypoint.php
@@ -2,6 +2,7 @@
 
 $github_token = getenv('GITHUB_API_TOKEN');
 if ($github_token) {
+    shell_exec("echo -e \"machine github.com\n  login x-access-token\n  password $github_token\" >> ~/.netrc");
     shell_exec("composer config -g http-basic.github.com x-access-token $github_token");
 }
 
